7c58546aed95cebb43adecd4d67f2e034a0653b2
Jonas Johansson <jns.johansson@gmail.com>
2024-01-11 19:55:43 +0100

    Downloads now per day

--- background.js
+++ background.js
@@ -9,18 +9,21 @@
 
 chrome.downloads.onDeterminingFilename.addListener(function(item, __suggest) {
   function suggest(filename, conflictAction) {
-    __suggest({filename: filename,
-               conflictAction: conflictAction,
-               conflict_action: conflictAction});
+    __suggest({
+      filename: filename,
+      conflictAction: conflictAction,
+      conflict_action: conflictAction
+    });
     // conflict_action was renamed to conflictAction in
     // https://chromium.googlesource.com/chromium/src/+/f1d784d6938b8fe8e0d257e41b26341992c2552c
     // which was first picked up in branch 1580.
   }
 
   var d = new Date();
-  var month = d.getMonth() + 1; // index starts at 0, so we have to add 1
-  var year = d.getFullYear();
+  var day = d.getUTCDate(); // Corrected method name
+  var month = d.getUTCMonth() + 1; // index starts at 0, so we have to add 1
+  var year = d.getUTCFullYear();
 
-  suggest(year + '/' + month + '/' + item.filename, 'uniquify');
+  suggest(year + '/' + month + '/' + day + '/' + item.filename, 'uniquify');
   return;
 });
